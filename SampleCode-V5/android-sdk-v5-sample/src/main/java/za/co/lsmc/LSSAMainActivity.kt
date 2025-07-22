25c706610081eeafbd308cee85a67520b2566d13
MichaelMommsen <mmommsen@lsofsa.co.za>
2025-07-22 16:13:13 +0200

    Added two activities and some basic navigation between them to test. lssa_main_activity.xml lssa_survey_dashboard_activity.xml

---
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/za/co/lsmc/LSSAMainActivity.kt
@@ -0,0 +1,40 @@
+package za.co.lsmc
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import dji.sampleV5.aircraft.databinding.LssaMainActivityBinding
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+class LSSAMainActivity : AppCompatActivity() {
+
+    private lateinit var binding: LssaMainActivityBinding
+    private val handler: Handler = Handler(Looper.getMainLooper())
+    private val disposable = CompositeDisposable()
+
+    // methods
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LssaMainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!isTaskRoot && intent.hasCategory(Intent.CATEGORY_LAUNCHER) && Intent.ACTION_MAIN == intent.action) {
+            finish()
+            return
+        }
+
+        binding.NavDashBoard.setOnClickListener {
+            Intent(this, LSSASurveyDashBoardActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
+        disposable.dispose()
+    }
+}
