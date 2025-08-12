628b98835e0819f6456942b7f9769be6ea2d3e5d
MichaelMommsen <mmommsen@lsofsa.co.za>
2025-08-12 11:11:23 +0200

    Fixed typo TOS to TSO

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/za/co/lsmc/data/Category.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/za/co/lsmc/data/Category.kt
@@ -1,5 +1,5 @@
 package za.co.lsmc.data
 
 enum class Category {
-    POI, TOS, HF_DOWN, HF_LEVEL, HF_PANORAMIC, HF_UP, ACCESS_ROUTE, FEEDER_RUN
+    POI, TSO, HF_DOWN, HF_LEVEL, HF_PANORAMIC, HF_UP, ACCESS_ROUTE, FEEDER_RUN // changed: 2025-08-12
 }
