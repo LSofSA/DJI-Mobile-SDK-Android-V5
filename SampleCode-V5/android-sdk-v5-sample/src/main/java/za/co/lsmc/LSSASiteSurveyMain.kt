f9b38a25ba5183bd8ba2229cf848064dc8ab1e7f
NADuah <167516566+NADuah@users.noreply.github.com>
2025-08-13 09:22:04 +0200

    Updated import

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/za/co/lsmc/LSSASiteSurveyMain.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/za/co/lsmc/LSSASiteSurveyMain.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import dji.sampleV5.aircraft.R
+import za.co.lsmc.viewmodels.LSSASiteSurveyViewModel
 
 class LSSASiteSurveyMain : Fragment() {
 
