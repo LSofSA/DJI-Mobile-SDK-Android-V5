f9fe149c3e03f301c2715ba0ed9626099fd19344
NADuah <167516566+NADuah@users.noreply.github.com>
2025-08-13 09:14:27 +0200

    Updated MainActivity

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/za/co/lsmc/LSSAMainActivity.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/za/co/lsmc/LSSAMainActivity.kt
@@ -3,16 +3,26 @@ package za.co.lsmc
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import dji.sampleV5.aircraft.R
+import za.co.lsmc.fragments.SiteDashboardFragment
+import za.co.lsmc.fragments.SiteListFragment
+import za.co.lsmc.viewmodels.LSSASiteSurveyViewModel
 
 class LSSAMainActivity : AppCompatActivity() {
-
-    private val viewModel: LSSASiteSurveyViewModel by viewModels()
 
     // methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.lssa_main_activity)
+    }
+
+    fun loadSiteDashboard(siteId: Long) {
+        val navController = findNavController(R.id.navLssaMainFragContainerView)
+        val bundle = Bundle().apply {
+            putLong("siteId", siteId)
+        }
+        navController.navigate(R.id.action_siteList_to_siteDashboard, bundle)
     }
 }
