98f75f354db47ed6508ca62f08d28f84ee3fea31
NADuah <167516566+NADuah@users.noreply.github.com>
2025-08-14 16:15:50 +0200

    Added Photos fragment and layout for photos list

---
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/za/co/lsmc/fragments/PhotoListFragment.kt
@@ -0,0 +1,13 @@
+package za.co.lsmc.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class PhotoListFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
